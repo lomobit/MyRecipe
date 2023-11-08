9d72c419c1e8fd60db556b26b502eb14adc3ae2a
lomobit <lomobit@gmail.com>
2023-11-08 16:59:42 +0300

    Дополнение скелета пайплайна запроса для событий

--- src/Application/MyRecipe.Handlers/Event/EventAddCommandHandler.cs
+++ src/Application/MyRecipe.Handlers/Event/EventAddCommandHandler.cs
@@ -1,11 +1,17 @@
 ﻿using MediatR;
+using MyRecipe.AppServices.Event;
 using MyRecipe.Handlers.Contracts.Event;
 
 namespace MyRecipe.Handlers.Event;
 
 public class EventAddCommandHandler : IRequestHandler<EventAddCommand, int>
 {
-    
+    public IEventService _eventService;
+
+    public EventAddCommandHandler(IEventService eventService)
+    {
+        _eventService = eventService;
+    }
     public async Task<int> Handle(EventAddCommand request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
--- src/Infrastructure/MyRecipe.Infrastructure/Repositories/Event/EventRepository.cs
+++ src/Infrastructure/MyRecipe.Infrastructure/Repositories/Event/EventRepository.cs
@@ -2,5 +2,10 @@
 
 public class EventRepository : IEventRepository
 {
-    
+    private readonly MyRecipeDbContext _context;
+
+    public EventRepository(MyRecipeDbContext context)
+    {
+        _context = context;
+    }
 }
