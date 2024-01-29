5dacf3d1c64da99e7b29c75a7101fb57aa26a53b
lomobit <lomobit@gmail.com>
2024-01-29 13:37:32 +0300

    Переименование поля в бд для таблицы UserStates

---
+++ src/Infrastructure/MyRecipe.Infrastructure/Migrations/20240129103648_RenameFieldForUserState.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyRecipe.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameFieldForUserState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FistName",
+                table: "UserStates",
+                newName: "FirstName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "UserStates",
+                newName: "FistName");
+        }
+    }
+}
